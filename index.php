ac01cc47e35e9bfd11e81a92d878b4acd5c9c5db
Will Stewart <william.stewart@students.plymouth.ac.uk>
2021-01-06 16:45:09 +0000

    Updated index page

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-    <?php
-    // Tutorial says put code in here
-    ?>
-    </body>
-</html>
+<html lang="en">
+<head>
+    <title>Llafan Hotel Booking System</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
+
+<body>
+
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <h1 style="font-family:Tahoma;white-space: nowrap; font-size: 40px;">Llafan Hotel Booking System</h1>
+</div>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">LHBS</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active" style="font-family: Calibri"><a href="#">Home</a></li>
+                <li style="font-family: Calibri"><a href="#">Page 2</a></li>
+                <li style="font-family: Calibri"><a href="#">Page 3</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+</body>
+</html>
