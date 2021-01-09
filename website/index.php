1bb0d162e8f238d9ce32a8e1927c81efbd2b4d78
Will Stewart <william.stewart@students.plymouth.ac.uk>
2021-01-09 16:12:18 +0000

    update template index navbar

--- website/index.php
+++ website/index.php
@@ -4,38 +4,61 @@
     <title>Llafan Hotel Booking System</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-
-
 <body>
 
 
-<div class="jumbotron text-center" style="margin-bottom:0">
-    <h1 style="font-family:Tahoma;white-space: nowrap; font-size: 40px;">Llafan Hotel Booking System</h1>
-</div>
 
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">LHBS</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li class="active" style="font-family: Calibri"><a href="index.php">Home</a></li>
-                <li style="font-family: Calibri"><a href="loginAdmin.php">Admin Login</a></li>
-                <li style="font-family: Calibri"><a href="loginUser.php">User Login</a></li>
+    <nav class="navbar navbar-expand-md navbar-dark " style="background-color: #8B0808;">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <img src="img/talyllyn-logo.png" width="228" height="66" alt="logo">
+                </li>
+            </ul>
+
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="loginUser.php">User Login</a>
+                </li>
+                <li class="nav-item" style="align-items: center">
+                    <a class="nav-link" href="loginAdmin.php">Admin Login</a>
+                </li>
             </ul>
         </div>
-    </div>
-</nav>
+
+
+
+        <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto" href="#">Tallwyn Railway</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+
+
+
+
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Right</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
 
 
 </body>
