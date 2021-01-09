1799e49e6d5d5b1d2eaef1206b6e08ac0b2dcc42
Will Stewart <william.stewart@students.plymouth.ac.uk>
2021-01-09 14:32:40 +0000

    add php login page

---
+++ website/loginAdmin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Llafan Hotel Booking System</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <h1 style="font-family:Tahoma;white-space: nowrap; font-size: 40px;">Llafan Hotel Booking System</h1>
+</div>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">LHBS</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active" style="font-family: Calibri"><a href="#">Home</a></li>
+                <li style="font-family: Calibri"><a href="#">Admin Login</a></li>
+                <li style="font-family: Calibri"><a href="#">User Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+</body>
+</html>
