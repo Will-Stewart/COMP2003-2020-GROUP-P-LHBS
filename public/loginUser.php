723817ab60a874dc89834e77d91e19952939cc2b
Adam Stewart <57760871+adam-stew@users.noreply.github.com>
2021-03-05 00:33:44 +0000

    Revert "Updated Sign and Log in page"
    This reverts commit a8c97672ed5870955163158a023e54df82d0f265.

--- public/loginUser.php
+++ public/loginUser.php
@@ -13,79 +13,44 @@ include_once 'header.php';
         </div>
     </div>
 
-    <!--This is the user login form-->
-    <div class="container">
-        <br>  <p class="text-center">This is where LHBS users should login, Admins please go to the admin log in page at: <a href="loginAdmin.php"> Admin Login</a></p>
-        <hr>
+<!--This is the user login form-->
+<div class="container">
+    <br>  <p class="text-center">This is where LHBS users should login, Admins please go to the admin log in page at: <a href="loginAdmin.php"> Admin Login</a></p>
+    <hr>
 
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <article class="card-body">
+                    <a href="" class="float-right btn btn-outline-primary">Sign up</a>
+                    <h4 class="card-title mb-4 mt-1">Volunteer - Sign in</h4>
+                    <form>
+                        <div class="form-group">
+                            <label>Your username</label>
+                            <input type="text" class="form-control" placeholder="Username" id="">
+                        </div> <!-- form-group// -->
+                        <div class="form-group">
+                            <a class="float-right" href="#">Forgot?</a>
+                            <label>Your password</label>
+                            <input type="password" class="form-control" placeholder="********" id="">
+                        </div> <!-- form-group// -->
+                        <div class="form-group">
+                        </div> <!-- form-group// -->
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block"> Login  </button>
+                        </div> <!-- form-group// -->
+                    </form>
+                </article>
+            </div> <!-- card.// -->
 
-        <div class="row">
-            <div class="col-sm">
+        </aside> <!-- col.// -->
+        <aside class="col-sm-4">
 
-                <div class="card">
-                    <article class="card-body">
-                        <h4 class="card-title mb-4 mt-1">Sign in</h4>
-                        <form>
-                            <div class="form-group">
-                                <label>Your username</label>
-                                <input type="text" class="form-control" placeholder="Username" id="username">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                                <a class="float-right" href="#">Forgot?</a>
-                                <label>Your password</label>
-                                <input type="password" class="form-control" placeholder="********" id="password">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block"> Login  </button>
-                            </div> <!-- form-group// -->
-                        </form>
-                    </article>
-                </div> <!-- card.// -->
-
-            </div>
-            <div class="col-sm">
-
-                <div class="card">
-                    <article class="card-body">
-                        <h4 class="card-title mb-4 mt-1">Sign up</h4>
-                        <form>
-                            <div class="form-group">
-                                <label>Your username</label>
-                                <input type="text" class="form-control" placeholder="Username" id="username">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                                <label>Your email</label>
-                                <input type="text" class="form-control" placeholder="Email" id="email">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                                <label>Your password</label>
-                                <input type="password" class="form-control" placeholder="********" id="password">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                                <label>Re Enter Your password</label>
-                                <input type="password" class="form-control" placeholder="********" id="repassword">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                            </div> <!-- form-group// -->
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block"> Login  </button>
-                            </div> <!-- form-group// -->
-                        </form>
-                    </article>
-                </div> <!-- card.// -->
-
-
-            </div>
-        </div>
-
-
-
-
-
-
+        </aside>
     </div>
+
+
+</div>
 </html>
 
 <?php
