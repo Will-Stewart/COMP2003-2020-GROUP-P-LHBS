e000cdf9d0e854ef9216c655f84d97d53f81fb4a
Will Stewart <william.stewart@students.plymouth.ac.uk>
2021-03-15 15:17:25 +0000

    Upadate formattiong on createBooking.php

--- public/createBooking.php
+++ public/createBooking.php
@@ -5,19 +5,19 @@ include_once 'developmentNotice.php';
 
     <div class="container">
         <div class="row">
-            <asi class="card">
+            <aside class="card">
                 <article class="card-body">
-                    <form class="row g-3 needs-validation" novalidate>
+                    <form class="row g-3 needs-validation" novalidate style="padding: 8px">
                         <div class="col-md-4">
                             <label for="validationCustom01" class="form-label">First name</label>
-                            <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+                            <input type="text" class="form-control" id="validationCustom01" placeholder="Mark" required>
                             <div class="valid-feedback">
                                 Looks good!
                             </div>
                         </div>
                         <div class="col-md-4">
                             <label for="validationCustom02" class="form-label">Last name</label>
-                            <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+                            <input type="text" class="form-control" id="validationCustom02" placeholder="Otto" required>
                             <div class="valid-feedback">
                                 Looks good!
                             </div>
@@ -25,8 +25,7 @@ include_once 'developmentNotice.php';
                         <div class="col-md-4">
                             <label for="validationCustomUsername" class="form-label">Gender</label>
                             <div class="input-group has-validation">
-                                <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Male" aria-describedby="inputGroupPrepend" required>
                                 <div class="invalid-feedback">
                                     Please choose a gender.
                                 </div>
@@ -35,20 +34,23 @@ include_once 'developmentNotice.php';
 
                         <div class="col-md-4">
                             <label for="validationCustom03" class="form-label">Age</label>
-                            <input type="text" class="form-control" id="validationCustom03" required>
+                            <input type="text" class="form-control" id="validationCustom03" placeholder="25" required>
                             <div class="invalid-feedback">
                                 Please provide a valid Age.
                             </div>
                         </div>
                         <div class="col-md-4">
                             <label for="validationCustom05" class="form-label">Amount Of People</label>
-                            <input type="text" class="form-control" id="validationCustom05" required>
+                            <input type="text" class="form-control" id="validationCustom05" placeholder="4" required>
                             <div class="invalid-feedback">
                                 Please provide the amount of people.
                             </div>
-                            <div class="col-12">
-                                <button class="btn btn-primary" type="submit">Submit form</button>
+                        </div>
+
+                            <div class="col-4">
+                                <button class="btn btn-primary" type="submit" >Submit form</button>
                             </div>
+
                     </form>
                 </article>
         </div> <!-- card.// -->
