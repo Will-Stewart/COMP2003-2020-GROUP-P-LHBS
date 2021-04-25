c9def6b7fde1877fa89d7a8c6c73acfdfec2f7b6
samir <samirkarim@hotmail.co.uk>
2021-04-25 22:03:43 +0100

    Minor changes made to confirm bookings.php ToDo: Consider problems with price when changing the values Displaying values from each record Updating the edited values

--- public/confirmBookings.php
+++ public/confirmBookings.php
@@ -24,22 +24,26 @@ $resultAll = mysqli_query($con, $sql3);
 <script type="text/javascript">
 
     $(document).ready(function($) {
-        $("#table-row").click(function() {
+        $("#unconfirmedBookingsTable tr").click(function() {
             //variable to store the bookingID from the row clicked
-            var tempBookingID = $("#table-row:first").val();
+            var tempBookingID = $(this).children().closest("td").html();
 
             //php script to get all the values from the database where bookingID = tempBookingID
             // and store them in global variables
+            <?php
 
+            ?>
             //setting the values of the fields to the stored variables
             //$("#editBookingStartDate").val();
             //$("#editBookingEndDate").val();
-            $("#editName").val("tempName");
+            $("#editFirstNameName").val("tempName");
+            $("#editLastName").val("tempName");
             //$("#editBlueRoom").val();
             //$("#editYellowRoom").val();
             //$("#editGreenRoom").val();
             $("#editAge").val(tempBookingID);
             //$("#editClients").val();
+            $("#editPrice").val();
 
 
         });
@@ -51,7 +55,21 @@ $resultAll = mysqli_query($con, $sql3);
             <h3>Selected Booking Details</h3>
             <form>
                 <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Booking Start Date</label>
+                    <label for="colFormLabel" class="col-sm-2 col-form-label">First Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="editFirstName" placeholder="First Name">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="colFormLabel" class="col-sm-2 col-form-label">Last Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="editLastName" placeholder="Last Name">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                       <label for="colFormLabel" class="col-sm-2 col-form-label">Booking Start Date</label>
                     <div class="col-sm-10">
                         <input type="date" class="form-control" id="editBookingStartDate" placeholder="col-form-label">
                     </div>
@@ -64,12 +82,7 @@ $resultAll = mysqli_query($con, $sql3);
                     </div>
                 </div>
 
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Name</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="editName" placeholder="Your Name...">
-                    </div>
-                </div>
+
 
                 <fieldset class="form-group">
                     <div class="row">
@@ -104,12 +117,40 @@ $resultAll = mysqli_query($con, $sql3);
                     </div>
                 </div>
 
+                <fieldset class="form-group">
+                    <div class="row">
+                        <legend class="col-form-label col-sm-2 pt-0">Gender</legend>
+                        <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="editGenderMale" value="option1">
+                                <label class="form-check-label" for="gridRadios1">
+                                    Male
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="editGenderFemale" value="option2">
+                                <label class="form-check-label" for="gridRadios2">
+                                    Female
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+
                 <div class="form-group row">
                     <label for="colFormLabel" class="col-sm-2 col-form-label">Clients</label>
                     <div class="col-sm-4">
                         <input type="text" class="form-control" id="editClients">
                     </div>
                 </div>
+
+                <div class="form-group row">
+                    <label for="colFormLabel" class="col-sm-2 col-form-label">Price</label>
+                    <div class="col-sm-4">
+                        <input type="text" readonly class="form-control" id="editPrice" value="£xx.xx">
+                    </div>
+                </div>
+
                 <label></label>
                 <div class="form-group row">
                     <input class="btn btn-primary" type="submit" value="Confirm Changes">
@@ -142,7 +183,7 @@ $resultAll = mysqli_query($con, $sql3);
             <div class="col-sm">
                 <h3>Unconfirmed Bookings</h3>
                 <div class="list-group">
-                    <table class="table">
+                    <table class="table" id="unconfirmedBookingsTable">
                         <thead>
                         <tr class="table-confirm">
                             <th scope="col">Booking ID</th>
@@ -156,7 +197,7 @@ $resultAll = mysqli_query($con, $sql3);
                         <?php
                         if($result-> num_rows > 0){
                             while($row = $result-> fetch_assoc()){
-                                echo "<tr id='table-row'><td>". $row["BookingID"] ."</td><td>". $row["First_Name"] ."</td><td>". $row["Last_Name"]. "</td><td>". $row["Booking_StartDate"]. "</td><td>". $row["Booking_EndDate"]."</td></t>";
+                                echo "<tr><td>". $row["BookingID"] ."</td><td>". $row["First_Name"] ."</td><td>". $row["Last_Name"]. "</td><td>". $row["Booking_StartDate"]. "</td><td>". $row["Booking_EndDate"]."</td></t>";
                             }
                             echo "</table>";
                         }
