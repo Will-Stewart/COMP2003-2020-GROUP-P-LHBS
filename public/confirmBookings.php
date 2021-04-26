dd745ed6708f47a655f4754b790b8627e0e069ad
samir <samirkarim@hotmail.co.uk>
2021-04-26 21:34:44 +0100

    Minor changes made to confirm bookings.php -clickable feedback (cursor and hover) -all values displaying -discard changes ToDo: -table changes for confirmed bookings (possibly) -consider putting number of clients in table-->change nth child -Updating the edited values -general formatting

--- public/confirmBookings.php
+++ public/confirmBookings.php
@@ -12,8 +12,8 @@ $con = new mysqli($servername, $username, $password);
 $id = $_SESSION['RegIDs'];
 $BookingID = $_SESSION['BookingID'];
 
-$sql = "select BookingID, First_Name, Last_Name, Booking_StartDate, Booking_EndDate from comp2003_p.hostelbookings where RegID = $id ";
-$sql2 = "select BookingID, First_Name, Last_Name, Booking_StartDate, Booking_EndDate from comp2003_p.hostelbookings where Confirmation = 'Confirmed' ";
+$sql = "select BookingID, First_Name, Last_Name, Booking_StartDate, Booking_EndDate, Gender, Preferred_Room, Age, Price from comp2003_p.hostelbookings where RegID = $id ";
+$sql2 = "select BookingID, First_Name, Last_Name, Booking_StartDate, Booking_EndDate, Gender, Preferred_Room, Age, Price from comp2003_p.hostelbookings where Confirmation = 'Confirmed' ";
 $sql3 = "select * from comp2003_p.hostelbookings where BookingID = $BookingID";
 
 $result = mysqli_query($con, $sql);
@@ -26,25 +26,66 @@ $resultAll = mysqli_query($con, $sql3);
     $(document).ready(function($) {
         $("#unconfirmedBookingsTable tr").click(function() {
             //variable to store the bookingID from the row clicked
-            var tempBookingID = $(this).children().closest("td").html();
+            var tempBookingID = $(this).find("td:nth-child(1)").text();
+            var tempFirstName = $(this).find("td:nth-child(2)").text();
+            var tempLastName = $(this).find("td:nth-child(3)").text();
+            var tempBookingIn = $(this).find("td:nth-child(4)").text();
+            var tempBookingOut = $(this).find("td:nth-child(5)").text();
+            var tempRoomType = $(this).find("td:nth-child(6)").text();
+            var tempAge = $(this).find("td:nth-child(7)").text();
+            var tempGender = $(this).find("td:nth-child(8)").text();
+            var tempPrice = $(this).find("td:nth-child(9)").text();
+            //leaving out
 
-            //php script to get all the values from the database where bookingID = tempBookingID
-            // and store them in global variables
-            <?php
 
-            ?>
             //setting the values of the fields to the stored variables
-            //$("#editBookingStartDate").val();
-            //$("#editBookingEndDate").val();
-            $("#editFirstNameName").val("tempName");
-            $("#editLastName").val("tempName");
-            //$("#editBlueRoom").val();
-            //$("#editYellowRoom").val();
-            //$("#editGreenRoom").val();
-            $("#editAge").val(tempBookingID);
-            //$("#editClients").val();
-            $("#editPrice").val();
+            $("#editFirstName").val(tempFirstName);
+            $("#editLastName").val(tempLastName);
+            $("#editBookingStartDate").val(tempBookingIn);
+            $("#editBookingEndDate").val(tempBookingOut);
+            radiobtnBlue = document.getElementById("editBlueRoom");
+            radiobtnYellow = document.getElementById("editYellowRoom");
+            radiobtnGreen = document.getElementById("editGreenRoom");
+            radiobtnMale = document.getElementById("editGenderMale");
+            radiobtnFemale = document.getElementById("editGenderFemale");
 
+            if(tempRoomType === "Blue"){
+                radiobtnBlue.checked = true;
+                radiobtnYellow.checked = false;
+                radiobtnGreen.checked = false;
+            }
+            else if(tempRoomType === "Yellow"){
+                radiobtnYellow.checked = true;
+                radiobtnBlue.checked = false;
+                radiobtnGreen.checked = false;
+            }
+            else if(tempRoomType === "Green"){
+                radiobtnGreen.checked = true;
+                radiobtnYellow.checked = false;
+                radiobtnBlue.checked = false;
+            }
+            else{
+                radiobtnBlue.checked = false;
+                radiobtnYellow.checked = false;
+                radiobtnGreen.checked = false;
+            }
+
+            $("#editAge").val(tempAge);
+            if(tempGender == "Male") {
+                radiobtnMale.checked = true;
+                radiobtnFemale.checked = false;
+            }
+            else if(tempGender == "Female"){
+                radiobtnMale.checked = false;
+                radiobtnFemale.checked = true;
+            }
+            else{
+                radiobtnMale.checked = false;
+                radiobtnFemale.checked = false;
+            }
+            $("#editPrice").val("£" + tempPrice);
+            //!!consider taking out £ to make sure the value can
+            // be stored in the database (only floats)
 
         });
     });
@@ -89,19 +130,19 @@ $resultAll = mysqli_query($con, $sql3);
                         <legend class="col-form-label col-sm-2 pt-0">Rooms</legend>
                         <div class="col-sm-10">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="editBlueRoom" value="option1" checked>
+                                <input class="form-check-input" type="radio" name="gridRadios" value="option1" id="editBlueRoom">
                                 <label class="form-check-label" for="gridRadios1">
                                     Blue Room - (Women)
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="editYellowRoom" value="option2">
+                                <input class="form-check-input" type="radio" name="gridRadios" value="option2" id="editYellowRoom">
                                 <label class="form-check-label" for="gridRadios2">
                                     Yellow Room - (Men)
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="editGreenRoom" value="option3">
+                                <input class="form-check-input" type="radio" name="gridRadios" value="option3" id="editGreenRoom">
                                 <label class="form-check-label" for="gridRadios3">
                                     Green Room - (Couples)
                                 </label>
@@ -116,19 +157,19 @@ $resultAll = mysqli_query($con, $sql3);
                         <input type="text" class="form-control" id="editAge">
                     </div>
                 </div>
-
+                <br>
                 <fieldset class="form-group">
                     <div class="row">
                         <legend class="col-form-label col-sm-2 pt-0">Gender</legend>
                         <div class="col-sm-10">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="editGenderMale" value="option1">
+                                <input class="form-check-input" type="radio" name="gridRadios2" id="editGenderMale" value="option1">
                                 <label class="form-check-label" for="gridRadios1">
                                     Male
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="editGenderFemale" value="option2">
+                                <input class="form-check-input" type="radio" name="gridRadios2" id="editGenderFemale" value="option2">
                                 <label class="form-check-label" for="gridRadios2">
                                     Female
                                 </label>
@@ -137,23 +178,18 @@ $resultAll = mysqli_query($con, $sql3);
                     </div>
                 </fieldset>
 
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Clients</label>
-                    <div class="col-sm-4">
-                        <input type="text" class="form-control" id="editClients">
-                    </div>
-                </div>
 
                 <div class="form-group row">
                     <label for="colFormLabel" class="col-sm-2 col-form-label">Price</label>
                     <div class="col-sm-4">
-                        <input type="text" readonly class="form-control" id="editPrice" value="£xx.xx">
+                        <input type="text" readonly class="form-control" id="editPrice" value="£">
                     </div>
                 </div>
 
                 <label></label>
                 <div class="form-group row">
                     <input class="btn btn-primary" type="submit" value="Confirm Changes">
+                    <input class="btn btn-primary" type="reset" value="Discard Changes">
                 </div>
             </form>
         </div>
@@ -171,6 +207,7 @@ $resultAll = mysqli_query($con, $sql3);
             font-family: sans-serif;
             font-size: 16px;
             text-align: left;
+            cursor: pointer;
         }
         th{
             background-color: #2f3947;
@@ -183,7 +220,7 @@ $resultAll = mysqli_query($con, $sql3);
             <div class="col-sm">
                 <h3>Unconfirmed Bookings</h3>
                 <div class="list-group">
-                    <table class="table" id="unconfirmedBookingsTable">
+                    <table class="table table-hover"  id="unconfirmedBookingsTable">
                         <thead>
                         <tr class="table-confirm">
                             <th scope="col">Booking ID</th>
@@ -191,13 +228,18 @@ $resultAll = mysqli_query($con, $sql3);
                             <th scope="col">Last Name</th>
                             <th scope="col">Booking Start Date</th>
                             <th scope="col">Booking End Date</th>
+                            <th scope="col">Room Type</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Gender</th>
+                            <th scope="col">Price</th>
                         </tr>
                         </thead>
                         <tbody>
                         <?php
                         if($result-> num_rows > 0){
                             while($row = $result-> fetch_assoc()){
-                                echo "<tr><td>". $row["BookingID"] ."</td><td>". $row["First_Name"] ."</td><td>". $row["Last_Name"]. "</td><td>". $row["Booking_StartDate"]. "</td><td>". $row["Booking_EndDate"]."</td></t>";
+                                echo "<tr><td>". $row["BookingID"] ."</td><td>". $row["First_Name"] ."</td><td>". $row["Last_Name"]. "</td><td>". $row["Booking_StartDate"]. "</td><td>". $row["Booking_EndDate"]."</td>
+                                <td>". $row["Preferred_Room"]."</td><td>". $row["Age"] ."</td><td>". $row["Gender"]."</td><td>". $row["Price"]."</td></t>";
                             }
                             echo "</table>";
                         }
@@ -225,6 +267,9 @@ $resultAll = mysqli_query($con, $sql3);
                             <th scope="col">Last Name</th>
                             <th scope="col">Booking Start Date</th>
                             <th scope="col">Booking End Date</th>
+                            <th scope="col">Room Type</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Gender</th>
                         </tr>
                         </thead>
                         <tbody>
